132d7af88f3adf827907e404d577a06ff2b44a38
TA2k <tombox2020@gmail.com>
2022-10-26 21:16:48 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,58 @@
+![Logo](admin/garmin.png)
+
+# ioBroker.garmin
+
+[![NPM version](https://img.shields.io/npm/v/iobroker.garmin.svg)](https://www.npmjs.com/package/iobroker.garmin)
+[![Downloads](https://img.shields.io/npm/dm/iobroker.garmin.svg)](https://www.npmjs.com/package/iobroker.garmin)
+![Number of Installations](https://iobroker.live/badges/garmin-installed.svg)
+![Current version in stable repository](https://iobroker.live/badges/garmin-stable.svg)
+
+[![NPM](https://nodei.co/npm/iobroker.garmin.png?downloads=true)](https://nodei.co/npm/iobroker.garmin/)
+
+**Tests:** ![Test and Release](https://github.com/TA2k/ioBroker.garmin/workflows/Test%20and%20Release/badge.svg)
+
+## garmin adapter for ioBroker
+
+Adapter for Garmin Connect
+
+# Loginablauf
+
+Die Home net Mail und Passwort eingeben.
+
+## Steuern
+
+Werte können direkt unter homenet.0.id.status.xxx.value gesetzt werden
+
+## Diskussion und Fragen
+
+<https://forum.iobroker.net/topic/59365/test-adapter-bauknecht-home-net>
+
+## Changelog
+
+### 0.0.1
+
+-   (TA2k) initial release
+
+## License
+
+MIT License
+
+Copyright (c) 2022 TA2k <tombox2020@gmail.com>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
---
+++ main.js
@@ -0,0 +1,359 @@
+"use strict";
+
+/*
+ * Created with @iobroker/create-adapter v2.3.0
+ */
+
+// The adapter-core module gives you access to the core ioBroker functions
+// you need to create an adapter
+const utils = require("@iobroker/adapter-core");
+const axios = require("axios").default;
+const Json2iob = require("./lib/json2iob");
+const tough = require("tough-cookie");
+const qs = require("qs");
+const { HttpsCookieAgent } = require("http-cookie-agent/http");
+
+// Load your modules here, e.g.:
+// const fs = require("fs");
+
+class Garmin extends utils.Adapter {
+  /**
+   * @param {Partial<utils.AdapterOptions>} [options={}]
+   */
+  constructor(options) {
+    super({
+      ...options,
+      name: "garmin",
+    });
+    this.deviceArray = [];
+
+    this.json2iob = new Json2iob(this);
+    this.cookieJar = new tough.CookieJar();
+    this.requestClient = axios.create({
+      withCredentials: true,
+      httpsAgent: new HttpsCookieAgent({
+        cookies: {
+          jar: this.cookieJar,
+        },
+      }),
+    });
+  }
+
+  /**
+   * Is called when databases are connected and adapter received configuration.
+   */
+  async onReady() {
+    // Reset the connection indicator during startup
+    this.setState("info.connection", false, true);
+    if (this.config.interval < 0.5) {
+      this.log.info("Set interval to minimum 0.5");
+      this.config.interval = 0.5;
+    }
+    if (!this.config.username || !this.config.password) {
+      this.log.error("Please set username and password in the instance settings");
+      return;
+    }
+
+    this.updateInterval = null;
+    this.reLoginTimeout = null;
+    this.refreshTokenTimeout = null;
+    this.session = {};
+    this.subscribeStates("*");
+
+    this.log.info("Login to Garmin");
+    await this.login();
+    if (this.session.access_token) {
+      await this.getDeviceList();
+      await this.updateDevices();
+      this.updateInterval = setInterval(async () => {
+        await this.updateDevices();
+      }, this.config.interval * 60 * 1000);
+    }
+    this.refreshTokenInterval = setInterval(() => {
+      this.refreshToken();
+    }, this.session.expires_in * 1000);
+  }
+  async login() {
+    const form = await this.requestClient({
+      method: "get",
+      url: "https://sso.garmin.com/sso/signin?service=https%3A%2F%2Fconnect.garmin.com%2Fmodern%2F&webhost=https%3A%2F%2Fconnect.garmin.com%2Fmodern%2F&source=https%3A%2F%2Fconnect.garmin.com%2Fsignin%2F&redirectAfterAccountLoginUrl=https%3A%2F%2Fconnect.garmin.com%2Fmodern%2F&redirectAfterAccountCreationUrl=https%3A%2F%2Fconnect.garmin.com%2Fmodern%2F&gauthHost=https%3A%2F%2Fsso.garmin.com%2Fsso&locale=en_GB&id=gauth-widget&cssUrl=https%3A%2F%2Fconnect.garmin.com%2Fgauth-custom-v1.2-min.css&privacyStatementUrl=https%3A%2F%2Fwww.garmin.com%2Fen-GB%2Fprivacy%2Fconnect%2F&clientId=GarminConnect&rememberMeShown=true&rememberMeChecked=false&createAccountShown=true&openCreateAccount=false&displayNameShown=false&consumeServiceTicket=false&initialFocus=true&embedWidget=false&socialEnabled=false&generateExtraServiceTicket=true&generateTwoExtraServiceTickets=true&generateNoServiceTicket=false&globalOptInShown=true&globalOptInChecked=false&mobile=false&connectLegalTerms=true&showTermsOfUse=false&showPrivacyPolicy=false&showConnectLegalAge=false&locationPromptShown=true&showPassword=true&useCustomHeader=false&mfaRequired=false&performMFACheck=false&rememberMyBrowserShown=true&rememberMyBrowserChecked=false",
+      headers: {
+        accept: "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+        "user-agent":
+          "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.0 Safari/605.1.15",
+        "accept-language": "en-GB,en;q=0.9",
+        referer: "https://connect.garmin.com/",
+      },
+    })
+      .then((res) => {
+        this.log.debug(JSON.stringify(res.data));
+        return this.extractHidden(res.data);
+      })
+      .catch((error) => {
+        this.log.error(error);
+        error.response && this.log.error(JSON.stringify(error.response.data));
+      });
+    await this.requestClient({
+      method: "post",
+      url: "https://sso.garmin.com/sso/signin?service=https%3A%2F%2Fconnect.garmin.com%2Fmodern%2F&webhost=https%3A%2F%2Fconnect.garmin.com%2Fmodern%2F&source=https%3A%2F%2Fconnect.garmin.com%2Fsignin&redirectAfterAccountLoginUrl=https%3A%2F%2Fconnect.garmin.com%2Fmodern%2F&redirectAfterAccountCreationUrl=https%3A%2F%2Fconnect.garmin.com%2Fmodern%2F&gauthHost=https%3A%2F%2Fsso.garmin.com%2Fsso&locale=en_GB&id=gauth-widget&cssUrl=https%3A%2F%2Fconnect.garmin.com%2Fgauth-custom-v1.2-min.css&privacyStatementUrl=https%3A%2F%2Fwww.garmin.com%2Fen-GB%2Fprivacy%2Fconnect%2F&clientId=GarminConnect&rememberMeShown=true&rememberMeChecked=false&createAccountShown=true&openCreateAccount=false&displayNameShown=false&consumeServiceTicket=false&initialFocus=true&embedWidget=false&socialEnabled=false&generateExtraServiceTicket=true&generateTwoExtraServiceTickets=true&generateNoServiceTicket=false&globalOptInShown=true&globalOptInChecked=false&mobile=false&connectLegalTerms=true&showTermsOfUse=false&showPrivacyPolicy=false&showConnectLegalAge=false&locationPromptShown=true&showPassword=true&useCustomHeader=false&mfaRequired=false&performMFACheck=false&rememberMyBrowserShown=true&rememberMyBrowserChecked=false",
+      headers: {
+        Host: "sso.garmin.com",
+        accept: "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+        "content-type": "application/x-www-form-urlencoded",
+        origin: "https://sso.garmin.com",
+        "accept-language": "en-GB,en;q=0.9",
+        "user-agent":
+          "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.0 Safari/605.1.15",
+        referer:
+          "https://sso.garmin.com/sso/signin?service=https%3A%2F%2Fconnect.garmin.com%2Fmodern%2F&webhost=https%3A%2F%2Fconnect.garmin.com%2Fmodern%2F&source=https%3A%2F%2Fconnect.garmin.com%2Fsignin&redirectAfterAccountLoginUrl=https%3A%2F%2Fconnect.garmin.com%2Fmodern%2F&redirectAfterAccountCreationUrl=https%3A%2F%2Fconnect.garmin.com%2Fmodern%2F&gauthHost=https%3A%2F%2Fsso.garmin.com%2Fsso&locale=en_GB&id=gauth-widget&cssUrl=https%3A%2F%2Fconnect.garmin.com%2Fgauth-custom-v1.2-min.css&privacyStatementUrl=https%3A%2F%2Fwww.garmin.com%2Fen-GB%2Fprivacy%2Fconnect%2F&clientId=GarminConnect&rememberMeShown=true&rememberMeChecked=false&createAccountShown=true&openCreateAccount=false&displayNameShown=false&consumeServiceTicket=false&initialFocus=true&embedWidget=false&socialEnabled=false&generateExtraServiceTicket=true&generateTwoExtraServiceTickets=true&generateNoServiceTicket=false&globalOptInShown=true&globalOptInChecked=false&mobile=false&connectLegalTerms=true&showTermsOfUse=false&showPrivacyPolicy=false&showConnectLegalAge=false&locationPromptShown=true&showPassword=true&useCustomHeader=false&mfaRequired=false&performMFACheck=false&rememberMyBrowserShown=true&rememberMyBrowserChecked=false",
+      },
+      data: qs.stringify({
+        username: this.config.username,
+        password: this.config.password,
+        embed: "false",
+        csrf: form.csrf,
+        rememberme: "on",
+      }),
+    })
+      .then((res) => {
+        this.log.debug(JSON.stringify(res.data));
+
+        this.setState("info.connection", true, true);
+      })
+      .catch((error) => {
+        this.log.error(error);
+        error.response && this.log.error(JSON.stringify(error.response.data));
+      });
+  }
+
+  async getDeviceList() {
+    await this.requestClient({
+      method: "get",
+      url: "",
+    })
+      .then(async (res) => {
+        this.log.debug(JSON.stringify(res.data));
+
+        //this.log.info(`Found ${res.data} devices`);
+        const locations = res.data[this.session.accountId];
+        for (const location in locations) {
+          this.log.info(`Found ${locations[location].length} devices in location ${location}`);
+          for (const device of locations[location]) {
+            const id = device.SAID;
+
+            this.deviceArray.push(id);
+            const name = device.APPLIANCE_NAME + " " + device.CATEGORY_NAME;
+
+            await this.setObjectNotExistsAsync(id, {
+              type: "device",
+              common: {
+                name: name,
+              },
+              native: {},
+            });
+            await this.setObjectNotExistsAsync(id + ".remote", {
+              type: "channel",
+              common: {
+                name: "Remote Controls",
+              },
+              native: {},
+            });
+
+            const remoteArray = [{ command: "Refresh", name: "True = Refresh" }];
+            remoteArray.forEach((remote) => {
+              this.setObjectNotExists(id + ".remote." + remote.command, {
+                type: "state",
+                common: {
+                  name: remote.name || "",
+                  type: remote.type || "boolean",
+                  role: remote.role || "boolean",
+                  def: remote.def || false,
+                  write: true,
+                  read: true,
+                },
+                native: {},
+              });
+            });
+            await this.setObjectNotExistsAsync(id + ".general", {
+              type: "channel",
+              common: {
+                name: "General Information",
+              },
+              native: {},
+            });
+            this.json2iob.parse(id + ".general", device, { forceIndex: true });
+          }
+        }
+      })
+      .catch((error) => {
+        this.log.error(error);
+        error.response && this.log.error(JSON.stringify(error.response.data));
+      });
+  }
+
+  async updateDevices() {
+    const statusArray = [
+      {
+        path: "status",
+        url: "",
+        desc: "Status of the device",
+      },
+    ];
+    for (const id of this.deviceArray) {
+      for (const element of statusArray) {
+        const url = element.url.replace("$id", id);
+
+        await this.requestClient({
+          method: element.method || "get",
+          url: url,
+        })
+          .then(async (res) => {
+            this.log.debug(JSON.stringify(res.data));
+            if (!res.data) {
+              return;
+            }
+            const data = res.data;
+
+            const forceIndex = true;
+            const preferedArrayName = null;
+
+            this.json2iob.parse(id + "." + element.path, data, {
+              forceIndex: forceIndex,
+              write: true,
+              preferedArrayName: preferedArrayName,
+              channelName: element.desc,
+            });
+            // await this.setObjectNotExistsAsync(element.path + ".json", {
+            //   type: "state",
+            //   common: {
+            //     name: "Raw JSON",
+            //     write: false,
+            //     read: true,
+            //     type: "string",
+            //     role: "json",
+            //   },
+            //   native: {},
+            // });
+            // this.setState(element.path + ".json", JSON.stringify(data), true);
+          })
+          .catch((error) => {
+            if (error.response) {
+              if (error.response.status === 401) {
+                error.response && this.log.debug(JSON.stringify(error.response.data));
+                this.log.info(element.path + " receive 401 error. Refresh Token in 60 seconds");
+                this.refreshTokenTimeout && clearTimeout(this.refreshTokenTimeout);
+                this.refreshTokenTimeout = setTimeout(() => {
+                  this.refreshToken();
+                }, 1000 * 60);
+
+                return;
+              }
+            }
+            this.log.error(element.url);
+            this.log.error(error);
+            error.response && this.log.error(JSON.stringify(error.response.data));
+          });
+      }
+    }
+  }
+  extractHidden(body) {
+    const returnObject = {};
+    const matches = body.matchAll(/<input (?=[^>]* name=["']([^'"]*)|)(?=[^>]* value=["']([^'"]*)|)/g);
+    for (const match of matches) {
+      if (match[2] != null) {
+        returnObject[match[1]] = match[2];
+      }
+    }
+    return returnObject;
+  }
+  async refreshToken() {
+    this.log.debug("Refresh token");
+
+    await this.requestClient({
+      method: "post",
+      url: "",
+    })
+      .then((res) => {
+        this.log.debug(JSON.stringify(res.data));
+        this.session = res.data;
+        this.log.debug("Refresh successful");
+        this.setState("info.connection", true, true);
+      })
+      .catch(async (error) => {
+        this.log.error(error);
+        error.response && this.log.error(JSON.stringify(error.response.data));
+        this.setStateAsync("info.connection", false, true);
+      });
+  }
+
+  /**
+   * Is called when adapter shuts down - callback has to be called under any circumstances!
+   * @param {() => void} callback
+   */
+  onUnload(callback) {
+    try {
+      this.setState("info.connection", false, true);
+      this.refreshTimeout && clearTimeout(this.refreshTimeout);
+      this.reLoginTimeout && clearTimeout(this.reLoginTimeout);
+      this.refreshTokenTimeout && clearTimeout(this.refreshTokenTimeout);
+      this.updateInterval && clearInterval(this.updateInterval);
+      this.refreshTokenInterval && clearInterval(this.refreshTokenInterval);
+      callback();
+    } catch (e) {
+      callback();
+    }
+  }
+
+  /**
+   * Is called if a subscribed state changes
+   * @param {string} id
+   * @param {ioBroker.State | null | undefined} state
+   */
+  async onStateChange(id, state) {
+    if (state) {
+      if (!state.ack) {
+        const deviceId = id.split(".")[2];
+        const command = id.split(".")[5];
+
+        if (id.split(".")[4] === "Refresh") {
+          this.updateDevices();
+          return;
+        }
+        const data = {
+          body: {},
+          header: {
+            command: "setAttributes",
+            said: deviceId,
+          },
+        };
+        data.body[command] = state.val;
+        await this.requestClient({
+          method: "post",
+          url: "",
+        })
+          .then((res) => {
+            this.log.info(JSON.stringify(res.data));
+          })
+          .catch(async (error) => {
+            this.log.error(error);
+            error.response && this.log.error(JSON.stringify(error.response.data));
+          });
+        this.refreshTimeout = setTimeout(async () => {
+          this.log.info("Update devices");
+          await this.updateDevices();
+        }, 10 * 1000);
+      }
+    }
+  }
+}
+
+if (require.main !== module) {
+  // Export the constructor in compact mode
+  /**
+   * @param {Partial<utils.AdapterOptions>} [options={}]
+   */
+  module.exports = (options) => new Garmin(options);
+} else {
+  // otherwise start the instance directly
+  new Garmin();
+}
